49f58419fefac67a2228bec3ee811161d14546cb
Pranav2612000 <pranav2000joglekar@gmail.com>
2024-06-25 08:34:22 +0530

    feat: save chunks to chromaDB

--- create_db.py
+++ create_db.py
@@ -1,8 +1,19 @@
 from dotenv import load_dotenv
 from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+import openai
+import os
+import shutil
+
+load_dotenv()
+openai.api_key = os.environ['OPENAI_API_KEY']
+openai.model = "text-embedding-3-small"
+print(os.environ['OPENAI_API_KEY'])
 
 DATA_PATH = "data/docs"
+CHROMA_PATH = "chroma"
 
 def main():
     print("Starting DB Generation...")
@@ -14,6 +25,9 @@ def main():
     print("Sample Chunk: ")
     print(chunks[7].page_content)
     print(chunks[7].metadata)
+
+    save(chunks)
+    print("DB Generation Complete.")
 
 def load_documents():
     loader = DirectoryLoader(DATA_PATH, glob="*.md", recursive=True)
@@ -31,6 +45,16 @@ def split_text(documents):
     print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
 
     return chunks;
+
+def save(chunks):
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+    db = Chroma.from_documents(
+        chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
+    )
+    db.persist()
+    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.");
 
 if __name__ == "__main__":
     main()
